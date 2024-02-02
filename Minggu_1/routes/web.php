c78904da7efe0c6d90737d8c2ca64159d64c1d49
yogaa24 <anangprayoga199@gmail.com>
2024-02-02 10:10:38 +0700

    ke 2

--- Minggu_1/routes/web.php
+++ Minggu_1/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+ Route::get('/about', function () {
+    return view ('about');
+ });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view ('servis');
+ });
