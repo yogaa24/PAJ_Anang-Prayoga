bfe9db80afb4587dd99fbcbf4d53c80ba010b075
yogaa24 <anangprayoga199@gmail.com>
2024-02-12 12:40:44 +0700

    ke 3

---
+++ Minggu_3/resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>about</title>
+</head>
+<body>
+    <h3>Ini Halaman About</h3>
+</body>
+</html>
+</head>
+<body>
+    <h2>Ini Halaman About</h2>
+</body>
+</html>
+</head>
+<body>
+    <h1>Ini Halaman About</h1>
+</body>
+</html>
